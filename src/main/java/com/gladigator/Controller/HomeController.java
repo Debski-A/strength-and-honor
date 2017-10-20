5ceb7cafe4b237b7fe9997408e61ab5a69113538
Adam Debski <adamdebski33@gmail.com>
2017-10-20 19:26:53 +0200

    Proba ogarniecia ExceptionHandlera tym razem bezposredni w HomeController, a nie przy pomocy Aspektu

--- src/main/java/com/gladigator/Controller/HomeController.java
+++ src/main/java/com/gladigator/Controller/HomeController.java
@@ -5,8 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.support.RequestContextUtils;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @Controller
 public class HomeController {
@@ -14,6 +13,12 @@ public class HomeController {
 	@GetMapping("/")
 	public String home(HttpServletRequest req) {
 		return "homepage";
+	}
+	
+	@ExceptionHandler(NoHandlerFoundException.class)
+	public String handleError404(Exception ex) {
+		
+		return "pageNotFound";
 	}
 	
 }
