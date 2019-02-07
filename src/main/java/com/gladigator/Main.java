a25cde44aac2821dde10df7225d313f9bc43d33e
Adam Debski <adamdebski33@gmail.com>
2019-02-07 20:13:03 +0100

    spring-boot ciag dalszy

--- src/main/java/com/gladigator/Main.java
+++ src/main/java/com/gladigator/Main.java
@@ -2,13 +2,12 @@ package com.gladigator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.ImportResource;
 
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication(exclude = { WebMvcAutoConfiguration.class , SecurityAutoConfiguration.class })
 @ImportResource(locations = {"classpath:com/gladigator/Configs/sah-servlet.xml", "classpath:com/gladigator/Configs/dao-context.xml", "classpath:com/gladigator/Configs/security-context.xml", "classpath:com/gladigator/Configs/service-context.xml"})
 public class Main {
 
