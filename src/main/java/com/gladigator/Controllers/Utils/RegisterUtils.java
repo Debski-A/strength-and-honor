265c59d1b0be37dfd9222a424d7f7a8ff8343213
Adam Debski <adamdebski33@gmail.com>
2019-02-10 12:03:07 +0100

    Nowa logika wysylania registration link

--- src/main/java/com/gladigator/Controllers/Utils/RegisterUtils.java
+++ src/main/java/com/gladigator/Controllers/Utils/RegisterUtils.java
@@ -1,10 +1,13 @@
 package com.gladigator.Controllers.Utils;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Locale;
 import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,11 +30,37 @@ public class RegisterUtils {
 	
 	private static final Logger LOG = LogManager.getLogger(RegisterUtils.class);
 	
+	private static final String SERVER_DOMAIN = "adam-debski.com";
+	
 	public String createLink(HttpServletRequest request, String confirmationToken) {
-		String appUrl = request.getScheme() + "://" + request.getServerName() + ":" + request.getLocalPort() + request.getContextPath();
+		//String appUrl = request.getScheme() + "://" + gethostName(request) + ":" + request.getLocalPort() + request.getContextPath();
+		String appUrl = obtainApplicationUrl(request);
 		appUrl += "/confirm?token=" + confirmationToken;
 		LOG.debug("Application URL with token param = {}", appUrl);
 		return appUrl;
+	}
+	
+	private String obtainApplicationUrl(HttpServletRequest request) {
+		String appUrl = null;
+		
+		String domainName = System.getProperty("domainName");
+		if (domainName == null) {
+			appUrl = request.getScheme() + "://" + request.getServerName() + ":" + request.getLocalPort() + request.getContextPath();
+		} else {
+			appUrl = request.getScheme() + "://" + domainName + request.getContextPath();
+		}
+		return appUrl;
+	}
+	
+	private String gethostName(HttpServletRequest request) {
+		String hostName = null;
+		try {
+			hostName = InetAddress.getLocalHost().getHostName();
+		} catch (UnknownHostException e) {
+			LOG.error(e);
+			hostName = request.getServerName();
+		}
+		return hostName;
 	}
 	
 	public String generateToken() {
