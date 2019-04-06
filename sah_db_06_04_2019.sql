318999ceb6b97b9c46c5da14ef1ec38d10a7a5d9
Adam Debski <adamdebski33@gmail.com>
2019-04-06 21:19:22 +0200

    sah_db newest script

--- sah_db_06_04_2019.sql
+++ sah_db_06_04_2019.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `sah_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `sah_db`;
 -- MySQL dump 10.13  Distrib 5.7.20, for Win64 (x86_64)
 --
 -- Host: localhost    Database: sah_db
@@ -48,7 +50,6 @@ DROP TABLE IF EXISTS `body_type_translations`;
 CREATE TABLE `body_type_translations` (
   `id_bt_translations` int(11) NOT NULL AUTO_INCREMENT,
   `language` varchar(45) CHARACTER SET utf8mb4 DEFAULT NULL,
-  `is_default` bit(1) DEFAULT NULL,
   `description` varchar(45) CHARACTER SET utf8mb4 DEFAULT NULL,
   `id_bt` int(11) NOT NULL,
   PRIMARY KEY (`id_bt_translations`),
@@ -64,7 +65,7 @@ CREATE TABLE `body_type_translations` (
 
 LOCK TABLES `body_type_translations` WRITE;
 /*!40000 ALTER TABLE `body_type_translations` DISABLE KEYS */;
-INSERT INTO `body_type_translations` (`id_bt_translations`, `language`, `is_default`, `description`, `id_bt`) VALUES (1,'en-UK','','none',1),(2,'en-UK','','ectomorph',2),(3,'en-UK','','mesomorph',3),(4,'en-UK','','endomorph',4),(5,'pl-PL','\0','nieokreślono',1),(6,'pl-PL','\0','ektomorficzny',2),(7,'pl-PL','\0','mezomorficzny',3),(8,'pl-PL','\0','endomorficzny',4);
+INSERT INTO `body_type_translations` (`id_bt_translations`, `language`, `description`, `id_bt`) VALUES (1,'en-GB','none',1),(2,'en-GB','ectomorph',2),(3,'en-GB','mesomorph',3),(4,'en-GB','endomorph',4),(5,'pl-PL','nieokreślono',1),(6,'pl-PL','ektomorficzny',2),(7,'pl-PL','mezomorficzny',3),(8,'pl-PL','endomorficzny',4);
 /*!40000 ALTER TABLE `body_type_translations` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -101,7 +102,6 @@ DROP TABLE IF EXISTS `frequency_of_activity_translations`;
 CREATE TABLE `frequency_of_activity_translations` (
   `id_foa_translations` int(11) NOT NULL AUTO_INCREMENT,
   `language` varchar(45) DEFAULT NULL,
-  `is_default` bit(1) DEFAULT NULL,
   `description` varchar(45) DEFAULT NULL,
   `id_foa` int(11) NOT NULL,
   PRIMARY KEY (`id_foa_translations`),
@@ -116,7 +116,7 @@ CREATE TABLE `frequency_of_activity_translations` (
 
 LOCK TABLES `frequency_of_activity_translations` WRITE;
 /*!40000 ALTER TABLE `frequency_of_activity_translations` DISABLE KEYS */;
-INSERT INTO `frequency_of_activity_translations` (`id_foa_translations`, `language`, `is_default`, `description`, `id_foa`) VALUES (1,'en-UK','','none',1),(2,'en-UK','','very low',2),(3,'en-UK','','low',3),(4,'en-UK','','medium',4),(5,'en-UK','','high',5),(6,'en-UK','','very high',6),(7,'pl-PL','\0','nieokreślono',1),(8,'pl-PL','\0','bardzo mała',2),(9,'pl-PL','\0','mała',3),(10,'pl-PL','\0','średnia',4),(11,'pl-PL','\0','duża',5),(12,'pl-PL','\0','bardzo duża',6);
+INSERT INTO `frequency_of_activity_translations` (`id_foa_translations`, `language`, `description`, `id_foa`) VALUES (1,'en-GB','none',1),(2,'en-GB','very low',2),(3,'en-GB','low',3),(4,'en-GB','medium',4),(5,'en-GB','high',5),(6,'en-GB','very high',6),(7,'pl-PL','nieokreślono',1),(8,'pl-PL','bardzo mała',2),(9,'pl-PL','mała',3),(10,'pl-PL','średnia',4),(11,'pl-PL','duża',5),(12,'pl-PL','bardzo duża',6);
 /*!40000 ALTER TABLE `frequency_of_activity_translations` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -129,8 +129,12 @@ DROP TABLE IF EXISTS `posts`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `posts` (
   `id_post` int(11) NOT NULL AUTO_INCREMENT,
+  `language` varchar(45) NOT NULL,
+  `content` mediumtext NOT NULL,
+  `latest_update` date NOT NULL,
+  `owner` varchar(45) NOT NULL,
   PRIMARY KEY (`id_post`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -139,36 +143,8 @@ CREATE TABLE `posts` (
 
 LOCK TABLES `posts` WRITE;
 /*!40000 ALTER TABLE `posts` DISABLE KEYS */;
-INSERT INTO `posts` (`id_post`) VALUES (6),(7),(8),(9),(10),(11),(12);
+INSERT INTO `posts` (`id_post`, `language`, `content`, `latest_update`, `owner`) VALUES (18,'pl-PL','<h1 style=\"text-align: center;\"><b><i><u>Drogi pamiętniczku...</u></i></b></h1><p>&nbsp; &nbsp;&nbsp;<img src=\"https://img.memecdn.com/no-title_o_444562.jpg\" alt=\"Znalezione obrazy dla zapytania java meme\"></p>','2019-03-09','admin'),(20,'pl-PL','<p>2</p>','2019-03-22','admin'),(21,'pl-PL','<p>3</p>','2019-03-22','admin'),(22,'pl-PL','<p>4</p>','2019-03-22','admin'),(23,'pl-PL','<p>hmmmmm</p>','2019-03-28','admin'),(24,'pl-PL','<p>hmm</p>','2019-03-28','admin');
 /*!40000 ALTER TABLE `posts` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `posts_translations`
---
-
-DROP TABLE IF EXISTS `posts_translations`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `posts_translations` (
-  `id_post_translations` int(11) NOT NULL AUTO_INCREMENT,
-  `language` varchar(45) DEFAULT NULL,
-  `is_default` bit(1) DEFAULT NULL,
-  `content` mediumtext NOT NULL,
-  `id_post` int(11) NOT NULL,
-  PRIMARY KEY (`id_post_translations`),
-  KEY `id_post_idx` (`id_post`),
-  CONSTRAINT `id_post` FOREIGN KEY (`id_post`) REFERENCES `posts` (`id_post`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `posts_translations`
---
-
-LOCK TABLES `posts_translations` WRITE;
-/*!40000 ALTER TABLE `posts_translations` DISABLE KEYS */;
-/*!40000 ALTER TABLE `posts_translations` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -228,7 +204,6 @@ DROP TABLE IF EXISTS `sex_translations`;
 CREATE TABLE `sex_translations` (
   `id_sex_translations` int(11) NOT NULL AUTO_INCREMENT,
   `language` varchar(45) CHARACTER SET utf8mb4 DEFAULT NULL,
-  `is_default` bit(1) DEFAULT NULL,
   `description` varchar(45) CHARACTER SET utf8mb4 DEFAULT NULL,
   `id_sex` int(11) NOT NULL,
   PRIMARY KEY (`id_sex_translations`),
@@ -243,7 +218,7 @@ CREATE TABLE `sex_translations` (
 
 LOCK TABLES `sex_translations` WRITE;
 /*!40000 ALTER TABLE `sex_translations` DISABLE KEYS */;
-INSERT INTO `sex_translations` (`id_sex_translations`, `language`, `is_default`, `description`, `id_sex`) VALUES (1,'en-UK','','none',1),(2,'en-UK','','male',2),(3,'en-UK','','female',3),(4,'pl-PL','\0','nieokreślono',1),(5,'pl-PL','\0','mężczyzna',2),(6,'pl-PL','\0','kobieta',3);
+INSERT INTO `sex_translations` (`id_sex_translations`, `language`, `description`, `id_sex`) VALUES (1,'en-GB','none',1),(2,'en-GB','male',2),(3,'en-GB','female',3),(4,'pl-PL','nieokreślono',1),(5,'pl-PL','mężczyzna',2),(6,'pl-PL','kobieta',3);
 /*!40000 ALTER TABLE `sex_translations` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -311,7 +286,7 @@ CREATE TABLE `users_details` (
 
 LOCK TABLES `users_details` WRITE;
 /*!40000 ALTER TABLE `users_details` DISABLE KEYS */;
-INSERT INTO `users_details` (`id_user`, `age`, `height`, `weight`, `id_foa`, `id_sex`, `id_bt`, `BMI`, `BMR`) VALUES (55,29,193,105,2,2,2,28.19,2539.5);
+INSERT INTO `users_details` (`id_user`, `age`, `height`, `weight`, `id_foa`, `id_sex`, `id_bt`, `BMI`, `BMR`) VALUES (55,29,193,121,6,2,2,28.19,NULL);
 /*!40000 ALTER TABLE `users_details` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -352,4 +327,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-02-21 20:36:01
+-- Dump completed on 2019-04-06 21:17:55
